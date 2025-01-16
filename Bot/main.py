6ff669ba607eab0710e801aea13ca3f321800424
Ivan24-wq <iv8747583@gmail.com>
2025-01-16 11:27:03 +0300

    add new keyboard

--- Bot/main.py
+++ Bot/main.py
@@ -1,5 +1,5 @@
 import asyncio
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, types, F
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.filters import Command
 import logging
@@ -21,7 +21,14 @@ dp = Dispatcher()
 #Кнопка старт
 start_keyboard = ReplyKeyboardMarkup(
     keyboard=[
-        [KeyboardButton(text="Бесплатные варианты")]
+        [KeyboardButton(text="Выберите подписку!")]
+    ],
+    resize_keyboard=True
+)
+#создание кнопок для оформления подписки
+finish_keyboard = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="Standart"), KeyboardButton(text="😎 PREMIUM")]
     ],
     resize_keyboard=True
 )
@@ -32,6 +39,32 @@ async def start_command(message: types.Message):
         "Рады вас видеть! \nВыберите свой тарифный план!",
         reply_markup=start_keyboard
     )
+@dp.message(F.text == "Standart")
+async def standart_subscribtion(message: types.Message):
+    await message.answer(
+        "Вы выбрали подписку Standart!. Ваш функционал ограничен."
+            
+    )
+
+
+        
+@dp.message(F.text == "😎 PREMIUM")
+async def standart_subscribtion(message: types.Message):
+    await message.answer(
+        "Вы выбрали подписку PREMIUM. Спасибо, что выбрали нашу команду!"
+            
+    )
+
+
+# Проработка кнопки "Выберите подписку"
+@dp.message(lambda message: message.text == "Выберите подписку!")
+async def command(message: types.Message):
+    await message.answer(
+        "Есть два варианты подписки. Standart - беспално, объявления доступны в пределах вашего региона. Premium - цена 220р., объявления доспупны в пределах города!",
+        reply_markup=finish_keyboard
+        
+    )
+
 
 async def main():
     await bot.delete_webhook(drop_pending_updates=True)
