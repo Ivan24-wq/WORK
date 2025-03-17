e5513c54f2d44c26bb1ee41c29ae6882b7f340c5
Ivan24-wq <iv8747583@gmail.com>
2025-03-17 08:48:35 +0300

    update bot

--- NewBot/Bot.py
+++ NewBot/Bot.py
@@ -1,9 +1,12 @@
 import asyncio
 from aiogram import Bot, Dispatcher, types
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.filters import Command
 from dotenv import load_dotenv  
 import os
 import logging
+
+
 load_dotenv()
 API_TOKEN = os.getenv("TELEGRAM_API_TOKEN")
 
@@ -14,16 +17,31 @@ logger = logging.getLogger(__name__)
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
 
+# Клавиатура
+start_keyboard = ReplyKeyboardMarkup(
+    keyboard=[[KeyboardButton(text="Выберите услугу!")]],
+    resize_keyboard=True
+)
 
+# Обработчик текстового сообщения "Привет"
+@dp.message(lambda message: message.text == "Привет")
+async def service(message: types.Message):
+    await message.answer("Привет! Выберите услугу:", reply_markup=start_keyboard)
+
+# Обработчик команды "/start"
 @dp.message(Command(commands=["start"]))
 async def start_command(message: types.Message):
-    await message.answer("Приветствуем! Рады вас видит дорогой клиент! Выберите услугу")
+    await message.answer(
+        "Приветствуем! Рады вас видеть, дорогой клиент! Выберите услугу:",
+        reply_markup=start_keyboard
+    )
 
-
+# Главная функция
 async def main():
     await bot.delete_webhook(drop_pending_updates=True)
     logger.info("Бот запущен!")
     await dp.start_polling(bot)
 
+# Запуск
 if __name__ == "__main__":
     asyncio.run(main())
